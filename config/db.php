ffe463d619aba9106c06c732d2d58c3d42d74384
Kila14 <kila14@yandex.ru>
2020-03-05 15:01:20 +0300

    Edit DB config.

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=eu-cdbr-west-02.cleardb.net;dbname=heroku_f999b9af44e8cd5',
+    'username' => 'bdaf6fb7597c2d',
+    'password' => '62d2e977',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
